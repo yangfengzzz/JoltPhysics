e3eb67d789a8c33443ede1bec3084ea6dc212406
yangfengzzz <yangfengzzz@hotmail.com>
2023-01-17 09:41:57 +0800

    first commit

---
+++ Build/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/yangfengzz/Desktop/JoltPhysics/Build
+# Build directory: /Users/yangfengzz/Desktop/JoltPhysics/Build/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(UnitTests "UnitTests")
+set_tests_properties(UnitTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/yangfengzz/Desktop/JoltPhysics/Build/CMakeLists.txt;293;add_test;/Users/yangfengzz/Desktop/JoltPhysics/Build/CMakeLists.txt;0;")
